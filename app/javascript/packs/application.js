04d437046870bc61284d2a53f9ab7ae9b2e1c5fa
Cobchise <cobchise@pm.me>
2020-12-10 21:20:20 -0500

    Added copy-to-clipboard urls

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+global.toastr = require("toastr")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -21,6 +22,12 @@ import '@fortawesome/fontawesome-free/js/all'
 import '../stylesheets/application'
 
 $(document).on('turbolinks:load', function() {
+    var clipboard = new ClipboardJS('.cb-btn')
+    clipboard.on('success', function(e) {
+        toastr.success("Text Copied")
+        e.clearSelection();
+    });
+
 
     (function() {
 
