6d9619f7e4040eb28e06d69843ac09834bd8f322
Cobchise <cobchise@pm.me>
2020-12-13 18:31:58 -0500

    Added region printing logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+    def printRegion(region, id)
+        case region
+        when "North America"
+            "<i class='fas fa-globe-americas mr-1'></i><span id='region-#{id}'>NA</span>"
+        when "Europe"
+            "<i class='fas fa-globe-europe mr-1'></i><span id='region-#{id}'>EU</span>"
+        when "Oceania"
+            "<i class='fas fa-globe-asia mr-1'></i><span id='region-#{id}'>OCE</span>"
+        else
+            "Unknown"
+        end
+    end
 end
